015d54839ed1be05593849054606901b0e6b8952
Hongbin Zhang <zhan2385@umn.edu>
2020-01-15 14:23:11 -0700

    wip

---
+++ main.cpp
@@ -0,0 +1,25 @@
+//The n-queens problem is amenable to encoding into SAT. Your task is to write a program that takes an integer n as input and finds a solution to the n-queens problem by encoding it into SAT.
+// Program input: An integer n > 0 entered as a command line argument to your binary or script.
+// Program output: A solution to the n-queens problem. (Note that I am looking for one solution and not all of them.) The output should be in the following format (example given for n = 4):
+
+int main(int argc, char const *argv[])
+{
+	// Process input
+	if (argc != 2)
+	{
+		printf("usage: nQueenSAT [number of queens 💅]\n");
+	}
+
+	// Convert input into minisat input
+
+	// Write input file
+
+	// Solve with minisat
+
+	// Read minisat output
+
+	// Output result to command line
+	
+
+	return 0;
+}
