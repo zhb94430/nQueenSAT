93f26c3cb15b93bb6f42476674fc78045d9eb275
Hongbin Zhang <zhan2385@umn.edu>
2020-01-15 11:46:46 -0700

    initial commit with minisat working

---
+++ minisat/core/depend.mk
@@ -0,0 +1,29 @@
+/Users/Peter/NonbrewableLibraries/minisat/core/Main.o /Users/Peter/NonbrewableLibraries/minisat/core/Main.or /Users/Peter/NonbrewableLibraries/minisat/core/Main.od /Users/Peter/NonbrewableLibraries/minisat/core/Main.op: /Users/Peter/NonbrewableLibraries/minisat/core/Main.cc \
+  /usr/local/include/zlib.h /usr/local/include/zconf.h \
+  /Users/Peter/NonbrewableLibraries/minisat/utils/System.h \
+  /Users/Peter/NonbrewableLibraries/minisat/mtl/IntTypes.h \
+  /Users/Peter/NonbrewableLibraries/minisat/utils/ParseUtils.h \
+  /Users/Peter/NonbrewableLibraries/minisat/utils/Options.h \
+  /Users/Peter/NonbrewableLibraries/minisat/mtl/Vec.h \
+  /Users/Peter/NonbrewableLibraries/minisat/mtl/XAlloc.h \
+  /Users/Peter/NonbrewableLibraries/minisat/core/Dimacs.h \
+  /Users/Peter/NonbrewableLibraries/minisat/core/SolverTypes.h \
+  /Users/Peter/NonbrewableLibraries/minisat/mtl/Alg.h \
+  /Users/Peter/NonbrewableLibraries/minisat/mtl/Map.h \
+  /Users/Peter/NonbrewableLibraries/minisat/mtl/Alloc.h \
+  /Users/Peter/NonbrewableLibraries/minisat/core/Solver.h \
+  /Users/Peter/NonbrewableLibraries/minisat/mtl/Heap.h
+/Users/Peter/NonbrewableLibraries/minisat/core/Solver.o /Users/Peter/NonbrewableLibraries/minisat/core/Solver.or /Users/Peter/NonbrewableLibraries/minisat/core/Solver.od /Users/Peter/NonbrewableLibraries/minisat/core/Solver.op: /Users/Peter/NonbrewableLibraries/minisat/core/Solver.cc \
+  /Users/Peter/NonbrewableLibraries/minisat/mtl/Sort.h \
+  /Users/Peter/NonbrewableLibraries/minisat/mtl/Vec.h \
+  /Users/Peter/NonbrewableLibraries/minisat/mtl/IntTypes.h \
+  /Users/Peter/NonbrewableLibraries/minisat/mtl/XAlloc.h \
+  /Users/Peter/NonbrewableLibraries/minisat/core/Solver.h \
+  /Users/Peter/NonbrewableLibraries/minisat/mtl/Heap.h \
+  /Users/Peter/NonbrewableLibraries/minisat/mtl/Alg.h \
+  /Users/Peter/NonbrewableLibraries/minisat/utils/Options.h \
+  /Users/Peter/NonbrewableLibraries/minisat/utils/ParseUtils.h \
+  /usr/local/include/zlib.h /usr/local/include/zconf.h \
+  /Users/Peter/NonbrewableLibraries/minisat/core/SolverTypes.h \
+  /Users/Peter/NonbrewableLibraries/minisat/mtl/Map.h \
+  /Users/Peter/NonbrewableLibraries/minisat/mtl/Alloc.h
