cfe6666bb45e85248c7aea14c1fb89d9fee710af
Hongbin Zhang <zhan2385@umn.edu>
2020-01-16 21:09:50 -0700

    Deliverable

--- main.cpp
+++ main.cpp
@@ -3,97 +3,99 @@
 // Program output: A solution to the n-queens problem. (Note that I am looking for one solution and not all of them.) The output should be in the following format (example given for n = 4):
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
 
 //Process current row/column/diag
-void ProcessCurrentArray(int* currentArray[]);
+void ProcessCurrentArray(std::vector<int>* currentArray, FILE* fp);
 
-string GenerateMiniSATInput(int numberOfQueens);
-void ProcessMiniSATOutput(string outputAddress);
+std::string GenerateMiniSATInput();
+void ProcessMiniSATOutput(std::string outputAddress);
 
-
-
+int termCount = 0;
+int numberOfQueens = 0;
 
 int main(int argc, char const *argv[])
 {
 	// Process input
 	if (argc != 2)
 	{
-		printf("usage: nQueenSAT [number of queens 💅]\n");
+		printf("Usage: nQueenSAT [number of queens 💅]\n");
+
+		return 0;
 	}
 
+	numberOfQueens = atoi(argv[1]);
+
 	// Convert input into minisat input
-	string inputAddress = GenerateMiniSATInput(int numberOfQueens);
+	std::string inputAddress = GenerateMiniSATInput();
 
-	// Solve with minisat
-	PROCESS_INFORMATION PI;
-	CreateProcess(NULL, "./minisat/minisat", something, something, something, inputAddress);
-	WaitForSingleObject(PI.hProcess, INFINITE);
+	//Solve with minisat
+	system("minisat ./minisatinput.txt ./minisatoutput.txt");
 
-	// Read minisat output
-	string outputAddress;
+	std::string outputAddress = "./minisatoutput.txt";
 
-	// Output result to command line
+	//Output result to command line
 	ProcessMiniSATOutput(outputAddress);
 
 	return 0;
 }
 
-void ProcessCurrentArray(int* currentArray[])
+void ProcessCurrentArray(std::vector<int>* currentArray, FILE* fp)
 {
 	//Simple Diag Check
 	//Only exec if not diagnal arrays
-	if ((currentArray[0] - currentArray[1]) == -1)
+	if ((currentArray->size() > 1) && ((currentArray->at(0) - currentArray->at(1)) == -1))
 	{
-		string currentLine;
+		std::string currentLine;
 
-		for (int i = 0; i < currentArray.size(); ++i)
+		for (int i = 0; i < currentArray->size(); ++i)
 		{
-			currentLine.append(currentArray[i]);
-			currentLine.append(" ");
+			currentLine += std::to_string(currentArray->at(i));
+			currentLine += " ";
 		}
 
-		currentLine.append("0");
+		currentLine += "0";
 
-		inputfp.fput(currentLine);
+		fprintf(fp, "%s\n", currentLine.c_str());
+		termCount++;
 	}
 
 	// Exclusive ors
-	for (int i = 0; i < currentArray.size(); ++i)
+	for (int i = 0; i < currentArray->size(); ++i)
 	{
-		for (int j = i+1; j < currentArray.size(); ++j)
+		for (int j = i+1; j < currentArray->size(); ++j)
 		{
-			string currentLine;
+			std::string currentLine;
 
-			currentLine.append("-");
-			currentLine.append(currentArray[i]);
-			currentLine.append(" -");
-			currentLine.append(currentArray[j]);
-			currentLine.append(" 0");
+			currentLine += "-";
+			currentLine += std::to_string(currentArray->at(i));
+			currentLine += " -";
+			currentLine += std::to_string(currentArray->at(j));
+			currentLine += " 0";
 
-			inputfp.fput(currentLine);
+			fprintf(fp, "%s\n", currentLine.c_str());
+			termCount++;
 		}
 	}
 }
 
-string GenerateMiniSATInput(int numberOfQueens)
+std::string GenerateMiniSATInput()
 {
-	string inputAddress = "./minisatinput";
-	FILE* input;
-	inputFP = fopen("./minisatinput", "w+");
+	std::string inputAddress = "./input.txt";
+	FILE* inputFP;
+	inputFP = fopen(inputAddress.c_str(), "w+");
 
 	if (inputFP == NULL)
 	{
 		printf("Fail to create minisat input file\n");
+
+		return NULL;
 	}
 	// Core of the assignment
 	else
 	{
-		//Calculate number of terms
-		int numberOfTerms = -1;
-
-		// Input header
-		inputFP.fput("p cnf %d %d\n", numberOfQueens*numberOfQueens, numberOfTerms);
-
 		// 1  2  3  4  5
 		// 6  7  8  9  10
 		// 11 12 13 14 15
@@ -101,76 +103,137 @@ string GenerateMiniSATInput(int numberOfQueens)
 		// 21 22 23 24 25
 
 		// Rows
-		for (unsigned i = 0; i <= numberOfQueens; ++i) 
+		for (int i = 0; i < numberOfQueens; ++i) 
 		{
-			int currentRow[];
+			std::vector<int> currentRow;
 
-			for(unsigned j = 0; j < numberOfQueens; ++j) {
+			for(int j = 0; j < numberOfQueens; ++j) 
+			{
 
-				currentRow[i] = i * numberOfQueens + j + 1; // Plus one to offset
+				currentRow.push_back(i * numberOfQueens + j + 1); // Plus one to offset
 			}
 
-			ProcessCurrentArray(currentRow);
+			ProcessCurrentArray(&currentRow, inputFP);
 		}
 
 		// Columns
-		for (unsigned i = 0; i <= numberOfQueens; ++i) 
+		for (int i = 0; i < numberOfQueens; ++i) 
 		{
-			int currentCol[];
+			std::vector<int> currentCol;
 
-			for(unsigned j = 0; j < numberOfQueens; ++j) {
+			for(int j = 0; j < numberOfQueens; ++j) 
+			{
 
-				currentCol[i] = j * numberOfQueens + i + 1; // Plus one to offset
+				currentCol.push_back(j * numberOfQueens + i + 1); // Plus one to offset
 			}
 
-			ProcessCurrentArray(currentCol);
+			ProcessCurrentArray(&currentCol, inputFP);
 		}
 
 		// Diag
 		// https://stackoverflow.com/questions/20420065/loop-diagonally-through-two-dimensional-array
-		for (unsigned k = 0; k < numberOfQueens * 2; ++k) 
+		for (int k = 0; k < numberOfQueens * 2; ++k) 
 		{
-			int currentDiag[];
+			std::vector<int> currentDiag;
 
-			for (unsigned j = 0; j <= k; ++j) 
+			for (int j = 0; j <= k; ++j) 
 			{
 				int i = k - j;
 
 				if (i < numberOfQueens && j < numberOfQueens)
 				{
-					currentDiag[j] = i * numberOfQueens + j + 1;
+					currentDiag.push_back(i * numberOfQueens + j + 1);
 				}
 			}
 
-			ProcessCurrentArray(currentDiag);
+			ProcessCurrentArray(&currentDiag, inputFP);
 		}
 	}
 
-	fclose(inputFP)
+	// Header
+	FILE* headerFP = fopen("./header.txt", "w+");
+	fprintf(headerFP, "p cnf %d %d\n", numberOfQueens*numberOfQueens, termCount);
+
+	fclose(inputFP);
+	fclose(headerFP);
+
+	system("cat ./header.txt ./input.txt > ./minisatinput.txt");
+
+	return inputAddress;
 }
 
-void ProcessMiniSATOutput(string outputAddress)
+void ProcessMiniSATOutput(std::string outputAddress)
 {
-	FILE* output;
-	outputFP = fopen("./minisatoutput", "r");
+	FILE* outputFP;
+	outputFP = fopen(outputAddress.c_str(), "r");
 
 	if (outputFP == NULL)
 	{
 		printf("Fail to open minisat output\n");
+
+		return;
 	}
 
 	else
 	{
-		if (output is unsatisfiable)
-		{
-			printf("unsatisfiable\n");
-		}
+		// Read the output file
+		fseek(outputFP, 0, SEEK_END);
+		size_t outputSize = ftell(outputFP);
+		char* output = new char[outputSize];
+		rewind(outputFP);
+		fread(output, sizeof(char), outputSize, outputFP);
+		std::string outputString(output);
 
 		// Satisfiable
-		// Result is all the positive numbers
+		if (outputString != std::string("UNSAT\n"))
+		{
+			// Process the output
+			std::string result = outputString.substr(4);
+			std::string tempString;
+			std::vector<int> convertedResult;
+
+			for (int i = 0; i < result.size(); ++i)
+			{
+				if (result[i] == std::string(" ")[0])
+				{
+					//Append and clear temp string
+					if (tempString != std::string(""))
+					{
+						convertedResult.push_back(atoi(tempString.c_str()));
+						tempString = "";
+					}
+				}
+
+				else
+				{
+					tempString.push_back(result[i]);
+				}
+			}
+
+			// Print the output
+			for (int i = 0; i < convertedResult.size(); ++i)
+			{
+				if (convertedResult[i] < 0)
+				{
+					printf("[ ]");
+				}
+				else
+				{
+					printf("[X]");
+				}
+
+				if ((i + 1) % numberOfQueens == 0 )
+				{
+					printf("\n");
+				}
+			}
+
+		}
+
+		// Unsatisfiable
 		else
 		{
-
+			printf("n = %d is unsatisfiable\n", numberOfQueens);
 		}
 	}
 
